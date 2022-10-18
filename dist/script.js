4f849b8826b591560201ce9587f6530dbf585490
LeonardoOPRH <113729750+LeonardoOPRH@users.noreply.github.com>
2022-10-17 22:35:30 -0300

    Operações Básicas

--- dist/script.js
+++ dist/script.js
@@ -1,43 +1,15 @@
-// var jogos = ["Geometria", "Situações Problema", "Fração", "Interpretação de Gráfico", "Números Decimais", "Tipos de Triângulo"];
-
-// for (var i = 0; i < jogos.length; i++) {
-//   document.write("<p>" + jogos[i] + "</p>");
-// }
-
-// Bebas Neue, Anton, Dangrek, Passion One, Acme, Russo One
-
-function operaçoesBasicas() {
-  // var numero1 = parseInt(Math.random() * 21);
-  // var numero2 = parseInt(Math.random() * 21);
-  // var resultado = numero1 + numero2;
-  // var operacao = [numero1, numero2, resultado];
-  // console.log("Operações Básicas");
-  // console.log(numero1);
-  // console.log(numero2);
-  // console.log(resultado);
-  var redirecionar = "<a href=''";
+function adicao() {
+  "<a href='https://cozy-biscochitos-f01b2d.netlify.app'";
 }
 
-function geometria() {
-  console.log("Geometria");
+function subtracao() {
+  "<a href=''";
 }
 
-function situacaoProblema() {
-  console.log("Situação Problema");
+function multiplicacao() {
+  "<a href=''";
 }
 
-function fracao() {
-  console.log("Fração");
-}
-
-function interpretacaoDeGrafico() {
-  console.log("Interpretação De Gráfico");
-}
-
-function numerosDecimais() {
-  console.log("Números Decimais");
-}
-
-function tiposDeTriangulos() {
-  console.log("Tipos De Triângulos");
+function divisao() {
+  "<a href=''";
 }
